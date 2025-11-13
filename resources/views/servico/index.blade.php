5ad48205fa7ebc545c8a14659e5bbfd8f37db426
isabelanista-hash <isabelanista@gmail.com>
2025-11-12 21:26:47 -0300

    Update index.blade.php

--- resources/views/servico/index.blade.php
+++ resources/views/servico/index.blade.php
@@ -1,26 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Lista de Serviços</title>
-</head>
-<body>
-    @extends('layouts.app')
+@extends('layouts.app')
 
-    @section('title', 'Lista de Serviços')
+@section('title', 'Lista de Serviços')
 
-    @section('content')
+@section('content')
 
     <h1 class="mb-4">Serviços Registrados</h1>
     <a href="{{ route('servico.create') }}" class="btn btn-primary mb-3">Adicionar Novo Serviço</a>
-        
-    </div>
+   
+    {{-- A <div> extra que estava aqui foi removida --}}
 
     @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
+        {{-- Adicionei a classe 'mt-3' aqui também para espaçar o alerta, caso ele apareça --}}
+        <div class="alert alert-success mt-3">{{ session('success') }}</div>
     @endif
-    
-    <table class="table table-striped table-bordered">
+   
+    {{--
+      AQUI ESTÁ A CORREÇÃO:
+      Adicionada a classe "mt-3" para criar a margem no topo da tabela.
+    --}}
+    <table class="table table-striped table-bordered mt-3">
         <thead class="table-dark">
             <tr>
                 <th>ID</th>
@@ -39,7 +37,7 @@
                 <td>{{ $servico->animal->nome ?? 'N/D' }} ({{ $servico->animal->raca ?? 'N/D' }})</td>
                 <td>{{ $servico->animal->cliente->nome ?? 'N/D' }}</td>
                 <td>R$ {{ number_format($servico->valor, 2, ',', '.') }}</td>
-                
+               
                 {{-- 🚨 COLUNA DE AÇÕES CORRIGIDA (Próximo passo) --}}
                 <td>
                     <a href="{{ route('servico.show', $servico->id) }}" class="btn btn-sm btn-info">
@@ -61,6 +59,3 @@
         </tbody>
     </table>
 @endsection
-    </table>
-</body>
-</html>
